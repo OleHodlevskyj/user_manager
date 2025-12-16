16b5c33f4b6fd6b4d59e3e8d46eaa1c60f34d8a4
OleHodlevskyj <godlevskyj04@gmail.com>
2025-12-16 19:21:20 +0200

    connecting to db

---
+++ api/db.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+$host = 'localhost';
+$db   = 'user_manager';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    echo('ok');
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode([
+        'status' => false,
+        'error'  => ['code' => 500, 'message' => 'Database connection error']
+    ]);
+    exit;
+}
