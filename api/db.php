2106dbb012fac6a20a07c44aff74d7e9534e265d
OleHodlevskyj <godlevskyj04@gmail.com>
2025-12-18 21:53:02 +0200

    for host

--- api/db.php
+++ api/db.php
@@ -1,9 +1,13 @@
 <?php
 
-$host = 'localhost';
-$db   = 'user_manager';
-$user = 'root';
-$pass = '';
+// $host = 'localhost';
+$host = 'sql213.infinityfree.com';
+
+// $db   = 'user_manager';
+$db   = 'if0_40714789_users';
+// $user = 'root';
+$user = 'if0_40714789';
+$pass = '89EJRXAm5bG';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
